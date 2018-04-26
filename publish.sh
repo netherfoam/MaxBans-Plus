387cb982a5f233685808c121ae5286970899ede1
Netherfoam <i.got.burnt@gmail.com>
2018-04-26 17:53:57 +1000

    fix(buildspec): only master will deploy an artefact now
    * fix(buildspec): try without quotes
    
    * it was whitespace all along
    
    * and back to normal

--- publish.sh
+++ publish.sh
@@ -3,9 +3,12 @@
 # This is a little awkward because of how CodeBuild clones the repository
 REMOTE_BRANCH=`git branch -a --contains HEAD | sed -n 2p`
 
+# Trim off the two leading characters (they're whitespace, after sed is used)
+REMOTE_BRANCH=${REMOTE_BRANCH:2}
+
 echo "REMOTE_BRANCH: $REMOTE_BRANCH"
 
-if [[ "master*" == "$REMOTE_BRANCH" ]]; then
+if [[ "master" == "$REMOTE_BRANCH" ]]; then
     echo "Publishing build..."
     # Upload the file because it's a commit directly to Master
     aws s3 cp target/maxbans-plus-*.jar s3://maxgamer.org/plugins/
